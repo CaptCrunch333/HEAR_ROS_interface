c4a9ffc5c82555c5cb3e5776f652034f4f0c991a
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-04-14 16:02:15 +0400

    added a line to update the constant

--- src/ROSUnit_SetFloatSrv.cpp
+++ src/ROSUnit_SetFloatSrv.cpp
@@ -15,7 +15,10 @@ bool(*ROSUnit_SetFloatSrv::callbackFunctionPointer[ROSUnit_capacity])(hear_msgs:
   ROSUnit_SetFloatSrv::srv_callback9,
   ROSUnit_SetFloatSrv::srv_callback10,
   ROSUnit_SetFloatSrv::srv_callback11,
-  ROSUnit_SetFloatSrv::srv_callback12
+  ROSUnit_SetFloatSrv::srv_callback12,
+  ROSUnit_SetFloatSrv::srv_callback13,
+  ROSUnit_SetFloatSrv::srv_callback14,
+  ROSUnit_SetFloatSrv::srv_callback15
 };
 
 Port* ROSUnit_SetFloatSrv::_output_port_0 = new OutputPort(ports_id::OP_0, NULL);
@@ -30,13 +33,17 @@ Port* ROSUnit_SetFloatSrv::_output_port_8 = new OutputPort(ports_id::OP_8, NULL)
 Port* ROSUnit_SetFloatSrv::_output_port_9 = new OutputPort(ports_id::OP_9, NULL);
 Port* ROSUnit_SetFloatSrv::_output_port_10 = new OutputPort(ports_id::OP_10, NULL);
 Port* ROSUnit_SetFloatSrv::_output_port_11 = new OutputPort(ports_id::OP_11, NULL);
+Port* ROSUnit_SetFloatSrv::_output_port_12 = new OutputPort(ports_id::OP_12, NULL);
+Port* ROSUnit_SetFloatSrv::_output_port_13 = new OutputPort(ports_id::OP_13, NULL);
+Port* ROSUnit_SetFloatSrv::_output_port_14 = new OutputPort(ports_id::OP_14, NULL);
+
 
 
 ROSUnit_SetFloatSrv::ROSUnit_SetFloatSrv(std::string t_name, ros::NodeHandle& t_main_handler) : ROSUnit(t_main_handler) {
     m_server = t_main_handler.advertiseService(t_name, ROSUnit_SetFloatSrv::callbackFunctionPointer[internal_counter]);
     m_ptr[internal_counter] = this;
     internal_counter++;
-    _ports = {_output_port_0, _output_port_1, _output_port_2, _output_port_3, _output_port_4, _output_port_5, _output_port_6, _output_port_7, _output_port_8, _output_port_9, _output_port_10, _output_port_11};
+    _ports = {_output_port_0, _output_port_1, _output_port_2, _output_port_3, _output_port_4, _output_port_5, _output_port_6, _output_port_7, _output_port_8, _output_port_9, _output_port_10, _output_port_11, _output_port_12, _output_port_13, _output_port_14};
 }
 
 ROSUnit_SetFloatSrv::~ROSUnit_SetFloatSrv() {
@@ -117,3 +124,24 @@ bool ROSUnit_SetFloatSrv::srv_callback12(hear_msgs::set_float::Request& req, hea
     _output_port_11->receiveMsgData(&t_msg);
     return true;
 }
+bool ROSUnit_SetFloatSrv::srv_callback13(hear_msgs::set_float::Request& req, hear_msgs::set_float::Response& res) {
+    FloatMsg t_msg;
+    t_msg.data = req.data;
+    _output_port_12->receiveMsgData(&t_msg);
+    return true;
+}
+
+bool ROSUnit_SetFloatSrv::srv_callback14(hear_msgs::set_float::Request& req, hear_msgs::set_float::Response& res) {
+    FloatMsg t_msg;
+    t_msg.data = req.data;
+    _output_port_13->receiveMsgData(&t_msg);
+    return true;
+}
+
+bool ROSUnit_SetFloatSrv::srv_callback15(hear_msgs::set_float::Request& req, hear_msgs::set_float::Response& res) {
+    FloatMsg t_msg;
+    t_msg.data = req.data;
+    _output_port_14->receiveMsgData(&t_msg);
+    return true;
+}
+
